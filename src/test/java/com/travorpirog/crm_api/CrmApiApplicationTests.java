1616ed0e9221812441e99c3e09487b19d8a726dd
Igor <traver.unimmer@gmail.com>
2023-11-10 17:53:24 +0300

    init: start crm_api project

---
+++ src/test/java/com/travorpirog/crm_api/CrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travorpirog.crm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
